5b71c4d0c9c262ada69c67879bb33983a7522a80
LaSeunghyun <rmwl2356@gmail.com>
2022-02-06 14:21:24 +0900

    terms커밋

---
+++ index/js/termsAgree.js
@@ -0,0 +1,109 @@
+'use strict';
+
+$(function () {
+    $('.open1').on('click', function (e) {
+        e.stopPropagation();
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.reservation').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open2').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.service').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open3').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.benefit').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open4').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.point').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open5').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.nav_all').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $("li[class *= 'open']").css({"border-bottom":"none"});
+    })
+    $('body').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $("li[class *= 'open']").css({"border-bottom":"none"});
+    })
+
+    $('.modal').hide();
+    $("li[class *= 'open']").on('click', function (e) {
+        e.stopPropagation();
+        $('.modal').fadeIn(200);
+    })
+    $(".nav_all").on('click', function (e) {
+        e.stopPropagation();
+        $('.modal').fadeOut(200);
+    })
+    $("body").on('click', function (e) {
+        e.stopPropagation();
+        $('.modal').fadeOut(200);
+    })
+    $('#agreeAll').click(function(){
+        let checked = $('#agreeAll').is(':checked');
+        if(checked){
+            $('input:checkbox').prop('checked', true);
+        }else{            
+            $('input:checkbox').prop('checked', false);
+        }
+    })
+    $('.false_modal').hide();
+    $('.confirm_phone, .confirm_ipin').click(function(){
+        let term1 = $('#term01').is(':checked');
+        let term2 = $('#term02').is(':checked');
+        let term3 = $('#term03').is(':checked');
+        
+        if(term1 && term2 && term3 == true){
+            
+        }else{
+            $('.false_modal').fadeIn(200);
+            $('.btn_cancel').click(function(){
+                $('.false_modal').fadeOut(200);
+            })
+        }
+    })
+});
