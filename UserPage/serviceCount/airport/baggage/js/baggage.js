3d5684b1627347b8afb48b56e4ff25c5282005e7
HWJB <cpzm012@gmail.com>
2022-02-10 02:16:19 +0900

    수하물 임시

---
+++ UserPage/serviceCount/airport/baggage/js/baggage.js
@@ -0,0 +1,87 @@
+'use strict';
+
+$(function () {
+    $('.open1').on('click', function (e) {
+        e.stopPropagation();
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.reservation').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open2').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.service').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open3').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.benefit').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open4').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.optional').slideUp(50);
+        $('.point').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.open5').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideDown("fast");
+        $(this).siblings().css({"border-bottom":"none"});
+        $(this).css({"border-bottom":"3px solid #652043"});
+    })
+    $('.nav_all').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $("li[class *= 'open']").css({"border-bottom":"none"});
+    })
+    $('body').on('click', function (e) {
+        e.stopPropagation();
+        $('.reservation').slideUp(50);
+        $('.service').slideUp(50);
+        $('.benefit').slideUp(50);
+        $('.point').slideUp(50);
+        $('.optional').slideUp(50);
+        $("li[class *= 'open']").css({"border-bottom":"none"});
+    })
+
+
+    $('.bag_save').on('click', function () {
+        $('#baggage1').css({"display":"block"});
+        $(this).css({"color": "#fff", "background-color": "rgb(102, 30, 67)"});
+        $('.bag_limit').css({"background-color":"white", "color": "#444"});
+        $('#baggage2').css({"display":"none"});
+    })
+    $('.bag_limit').on('click', function () {
+        $('#baggage2').css({"display":"block"});
+        $(this).css({"color": "#fff", "background-color": "rgb(102, 30, 67)"});
+        $('.bag_save').css({"background-color":"white", "color": "#444"});
+        $('#baggage1').css({"display":"none"});
+    })
+
+});
