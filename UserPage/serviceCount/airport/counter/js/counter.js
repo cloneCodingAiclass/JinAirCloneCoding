839783d6002211d26e4e479e53667f9274153841
LaSeunghyun <rmwl2356@gmail.com>
2022-02-17 16:08:56 +0900

    카운터 모달창, display 일부 수정

--- UserPage/serviceCount/airport/counter/js/counter.js
+++ UserPage/serviceCount/airport/counter/js/counter.js
@@ -84,125 +84,25 @@ $(function () {
         $('.modal').fadeOut(200);
     })
 
+    $('.icoFold1').click(function(){
+        $('.list1').css("display", "block");
+        $('.icoFold1').click(function(){
+            $('.list1').css("display", "none");
+        })
+    })
+    $('.icoFold2').click(function(){
+        $('.list2').css("display", "block");
+        $('.icoFold2').click(function(){
+            $('.list2').css("display", "none");
+        })
+    })
+    $('.icoFold3').click(function(){
+        $('.list3').css("display", "block");
+        $('.icoFold3').click(function(){
+            $('.list3').css("display", "none");
+        })
+    })
 });
-
-// trip-layer
-$(() => {
-    $(".trip_select_opt").on("click", function (e) {
-        e.stopPropagation();
-        $(".trip_layerbtn").css({ "color": "rgb(145, 0, 70)" });
-        $(".trip_down_img").css({ "display": "none" });
-        $(".trip_up_img").css({ "display": "inline-block" });
-        $(".trip_pop_layer").slideDown("fast");
-    })
-    $(".trip_down_img").on("click", function (e) {
-        e.stopPropagation();
-        $(".trip_layerbtn").css({ "color": "rgb(145, 0, 70)" });
-        $(".trip_down_img").css({ "display": "none" });
-        $(".trip_up_img").css({ "display": "inline-block" });
-        $(".trip_pop_layer").slideDown("fast");
-    })
-    $(".trip_up_img").on("click", function (e) {
-        e.stopPropagation();
-        $(".trip_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".trip_down_img").css({ "display": "inline-block" });
-        $(".trip_up_img").css({ "display": "none" });
-        $('.trip_pop_layer').slideUp(50);
-    })
-    $(".trip_type1").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".trip_type1").text();
-        $(".trip_select_opt").text(text);
-        $(".trip_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".trip_down_img").css({ "display": "inline-block" });
-        $(".trip_up_img").css({ "display": "none" });
-        $('.trip_pop_layer').slideUp(50);
-    })
-    $(".trip_type2").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".trip_type2").text();
-        $(".trip_select_opt").text(text);
-        $(".trip_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".trip_down_img").css({ "display": "inline-block" });
-        $(".trip_up_img").css({ "display": "none" });
-        $('.trip_pop_layer').slideUp(50);
-    })
-    $(".trip_type3").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".trip_type3").text();
-        $(".trip_select_opt").text(text);
-        $(".trip_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".trip_down_img").css({ "display": "inline-block" });
-        $(".trip_up_img").css({ "display": "none" });
-        $('.trip_pop_layer').slideUp(50);
-    })
-    $("#container").on('click', function (e) {
-        e.stopPropagation();
-        $(".trip_layerbtn").css({"color" : "rgb(0, 0, 0)"});
-        $(".trip_down_img").css({"display" : "inline-block"});
-        $(".trip_up_img").css({"display" : "none"});
-        $('.trip_pop_layer').slideUp(50);
-    })
-})
-
-// person-layer
-$(() => {
-    $(".person_select_opt").on("click", function (e) {
-        e.stopPropagation();
-        $(".person_layerbtn").css({ "color": "rgb(145, 0, 70)" });
-        $(".person_down_img").css({ "display": "none" });
-        $(".person_up_img").css({ "display": "inline-block" });
-        $(".person_pop_layer").slideDown("fast");
-    })
-    $(".person_down_img").on("click", function (e) {
-        e.stopPropagation();
-        $(".person_layerbtn").css({ "color": "rgb(145, 0, 70)" });
-        $(".person_down_img").css({ "display": "none" });
-        $(".person_up_img").css({ "display": "inline-block" });
-        $(".person_pop_layer").slideDown("fast");
-    })
-    $(".person_up_img").on("click", function (e) {
-        e.stopPropagation();
-        $(".person_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".person_down_img").css({ "display": "inline-block" });
-        $(".person_up_img").css({ "display": "none" });
-        $('.person_pop_layer').slideUp(50);
-    })
-    $(".person_type1").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".person_type1").text();
-        $(".person_select_opt").text(text);
-        $(".person_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".person_down_img").css({ "display": "inline-block" });
-        $(".person_up_img").css({ "display": "none" });
-        $('.person_pop_layer').slideUp(50);
-    })
-    $(".person_type2").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".person_type2").text();
-        $(".person_select_opt").text(text);
-        $(".person_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".person_down_img").css({ "display": "inline-block" });
-        $(".person_up_img").css({ "display": "none" });
-        $('.person_pop_layer').slideUp(50);
-    })
-    $(".person_type3").on("click", function (e) {
-        e.stopPropagation();
-        const text = $(".person_type3").text();
-        $(".person_select_opt").text(text);
-        $(".person_layerbtn").css({ "color": "rgb(0, 0, 0)" });
-        $(".person_down_img").css({ "display": "inline-block" });
-        $(".person_up_img").css({ "display": "none" });
-        $('.person_pop_layer').slideUp(50);
-    })
-    $("#container").on('click', function (e) {
-        e.stopPropagation();
-        $(".person_layerbtn").css({"color" : "rgb(0, 0, 0)"});
-        $(".person_down_img").css({"display" : "inline-block"});
-        $(".person_up_img").css({"display" : "none"});
-        $('.person_pop_layer').slideUp(50);
-    })
-})
 
 
 /* 컨테이너 js */
