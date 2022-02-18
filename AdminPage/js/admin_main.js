65561c602f55110112fae5b80c11b828d1a9ed06
HWJB <cpzm012@gmail.com>
2022-02-18 14:10:50 +0900

    어드민 업데이트
    02 18_ index, css. chart

--- AdminPage/js/admin_main.js
+++ AdminPage/js/admin_main.js
@@ -1,96 +1,105 @@
 $(function () {
-    $('.nav1').find('a').css({"color":"rgb(124,0,72)"});
+    $('.nav1').find('a').css({"color":"#BDD600"});
+    $('.nav1').siblings('li').css({"display":"block"});
+    $('.nav1').siblings('li').eq(0).find('a').css({"color":"rgb(124,0,72)"});
+    
     $('.nav1').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
+        $('.nav1').siblings('li').eq(0).find('a').css({"color":"rgb(124,0,72)"});
     });
     $('.nav2').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav3').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav4').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav5').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav6').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav7').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav8').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav9').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav10').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('.nav11').on('mouseover', function(e){
         e.stopPropagation();
-        $(this).parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $(this).find('a').css({"color":"rgb(124,0,72)"});
+        $(this).parent().siblings().find('a').css({"color":"#adb5bd"});
+        $(this).find('a').css({"color":"#BDD600"});
         $(this).siblings('li').css({"display":"block"});
         $(this).parent().siblings().find('li').css({"display":"none"});
     });
     $('header').on('mouseover', function (e) {
         e.stopPropagation();
-        $('.nav1').parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $('.nav1').find('a').css({"color":"rgb(124,0,72)"});
+        $('.nav1').parent().siblings().find('a').css({"color":"#adb5bd"});
+        $('.nav1').find('a').css({"color":"#BDD600"});
         $('.nav1').siblings('li').css({"display":"block"});
         $('.nav1').parent().siblings().find('li').css({"display":"none"});
     })
     $('.contents').on('mouseover', function (e) {
         e.stopPropagation();
-        $('.nav1').parent().siblings().find('a').css({"color":"rgb(0,0,0)"});
-        $('.nav1').find('a').css({"color":"rgb(124,0,72)"});
+        $('.nav1').parent().siblings().find('a').css({"color":"#adb5bd"});
+        $('.nav1').find('a').css({"color":"#BDD600"});
         $('.nav1').siblings('li').css({"display":"block"});
         $('.nav1').parent().siblings().find('li').css({"display":"none"});
     })
+
+});
+
+$(function () {
+
 
     let country = $('.other_select option:selected').val();
 
@@ -120,7 +129,7 @@ const dd = date.getDate();
 const hh = date.getHours();
 const mm = date.getMinutes();
 const ss = date.getSeconds();
-document.getElementById("korea_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + hh + ":" + mm + ":" + ss;
+document.getElementById("korea_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + ":" + mm + ":" + ss;
 }
 
 const st = window.setInterval(printtime);  // 입장시 시간 출력
