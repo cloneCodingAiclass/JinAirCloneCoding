d002cea7236873df24df770a265f652deda68647
LaSeunghyun <rmwl2356@gmail.com>
2022-02-19 20:19:10 +0900

    getReser 업데이트

--- UserPage/index/js/Mypage_getReservationDetail.js
+++ UserPage/index/js/Mypage_getReservationDetail.js
@@ -100,6 +100,7 @@ $(function () {
         $('.bbsbox2').css('display', 'block');
     });
 
+    /* 항공권 변경 */
     $('.reser_change_btn').click(function(){
         $('.reser_change').css('display', 'flex');
         $('.reser_change').fadeIn(200);
@@ -121,6 +122,7 @@ $(function () {
             })
         });
     });
+    /* 항공권 취소 */
     $('.reser_cancel_btn').click(function(){
         $('.reser_cancel').css('display', 'flex');
         $('.reser_cancel').fadeIn(200);
@@ -139,11 +141,26 @@ $(function () {
         });
     });
     
+    /* 부가서비스 */
+    $('.sub_service_btn').click(function(){
+        $('.sub_service').css('display', 'flex');
+        $('.sub_service').fadeIn(200);
+    })
+    $('.add_service').click(function(){
+        location.href="/UserPage/payment/extras.html";
+    })
+    $('.cancel_service').click(function(){
+        location.href="/UserPage/index/mypage/Mypage_cancel_service.html";
+    })
+    /* 공통 버튼 */
     $('.btn_cancel').click(function(){
         $('.reser_change').hide();
-        
+        $('.sub_service').hide();
         $('.reser_cancel').hide();
+        $('.trip_modal').hide();
+        $('.curr_info').hide();
     })
+    /* 모달 체크박스 전체 체크*/
     $('.peopleAll').click(function(){
         let checked = $('.peopleAll').is(':checked');
 
@@ -164,13 +181,33 @@ $(function () {
         }
     });
 
-    /* 부가서비스 */
-    $('add_service').click(function(){
-        location.href='/UserPage/payment/extras.html';
+    /* 여행가입 보험 */
+    $('.trip_btn').click(function(){
+        $('.trip_modal').css('display', 'flex');
+        $('.trip_modal').fadeIn(200);
+
+        $('.confirm_btn').click(function(){
+            $('.trip_false_modal').hide();
+        let check1 = $('.check1').is(':checked');
+        let check2 = $('.check2').is(':checked');
+
+        if(!check1 || !check2){
+            $('.trip_false_modal').css('display', 'flex');
+            $('.confirm_btn').click(function(){
+                $('.trip_false_modal').hide();
+            });
+        }else{
+            location.href="https://ec.aceinsurance.co.kr/"
+        }
     })
-    $('.cancel_service').click(function(){
-        
-    })
+    });
+
+    /* 탑승현황보기 */
+    $('.curr_btn').click(function(){
+        $('.curr_info').css('display', 'flex');
+        $('.curr_info').fadeIn(200);
+    });
+
 });
 
 
