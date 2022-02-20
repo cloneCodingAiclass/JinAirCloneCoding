0b1e4a4caf03ef4b3481ea67c3345e00736eff8e
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-02-20 16:33:42 +0900

    관리자 인덱스 완료

--- AdminPage/js/admin_main.js
+++ AdminPage/js/admin_main.js
@@ -99,36 +99,225 @@ $(function () {
 
 $(function () {
 
-
-    let country = $('.other_select option:selected').val();
-
-    switch(country){
-        case '부다페스트' :
-            function printtime(){
-                const date = new Date();
-                const yyyy = date.getFullYear();
-                const mo = date.getMonth()+1;
-                const dd = date.getDate();
-                const hh = date.getHours()-8;
-                const mm = date.getMinutes();
-                const ss = date.getSeconds();
-                document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + hh + ":" + mm + ":" + ss;
-            }
-                break;
+    function printtime(){ // 부다페스트
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-8;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
     }
-    const st = window.setInterval(printtime);
+    function printtime1(){ // 런던
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-9;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime2(){ // 뉴욕
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-14;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime3(){ // 베이징
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-1;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime4(){ // 하노이
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-2;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime5(){ // 두바이
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-5;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime6(){ // 도하
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-6;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    function printtime7(){ // 카이로
+        let date = new Date();
+        let yyyy = date.getFullYear();
+        let mo = date.getMonth()+1;
+        if(mo < 10) mo = `0${mo}`;
+        let dd = date.getDate();
+        let hh = date.getHours()-7;
+        if(hh < 0){
+            dd = dd-1;
+            hh = hh+ 24;
+            if(dd < 10) dd = `0${dd}`;
+        }
+        if(hh < 10) hh = `0${hh}`;
+        let mm = date.getMinutes();
+        if(mm < 10) mm = `0${mm}`;
+        let ss = date.getSeconds();
+        if(ss < 10) ss = `0${ss}`;
+        document.getElementById("other_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
+    }
+    let st;
+    let st1;
+    let st2;
+    let st3;
+    let st4;
+    let st5;
+    let st6;
+    let st7;
+
+    st2 = window.setInterval(printtime2);
+        
+        $('#other_select').on('change', function(){
+            window.clearInterval(st);   
+            window.clearInterval(st1); 
+            window.clearInterval(st2); 
+            window.clearInterval(st3); 
+            window.clearInterval(st4); 
+            window.clearInterval(st5); 
+            window.clearInterval(st6); 
+            window.clearInterval(st7); 
+
+                if($('#other_select').val()=='부다페스트'){
+                    console.log($('#other_select').val());
+                    st = window.setInterval(printtime);
+                }else if($('#other_select').val()=='런던'){
+                    console.log($('#other_select').val());
+                    st1 = window.setInterval(printtime1);
+                }else if($('#other_select').val()=='뉴욕'){
+                    console.log($('#other_select').val());
+                    st2 = window.setInterval(printtime2);
+                }else if($('#other_select').val()=='베이징'){
+                    console.log($('#other_select').val());
+                    st3 = window.setInterval(printtime3);
+                }else if($('#other_select').val()=='하노이'){
+                    console.log($('#other_select').val());
+                    st4 = window.setInterval(printtime4);
+                }else if($('#other_select').val()=='두바이'){
+                    console.log($('#other_select').val());
+                    st5 = window.setInterval(printtime5);
+                }else if($('#other_select').val()=='도하'){
+                    console.log($('#other_select').val());
+                    st6 = window.setInterval(printtime6);
+                }else if($('#other_select').val()=='카이로'){
+                    console.log($('#other_select').val());
+                    st7 = window.setInterval(printtime7);
+                }
+
+        })
+
+
 
 });
 
 function printtime(){
-const date = new Date();
-const yyyy = date.getFullYear();
-const mo = date.getMonth()+1;
-const dd = date.getDate();
-const hh = date.getHours();
-const mm = date.getMinutes();
-const ss = date.getSeconds();
-document.getElementById("korea_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + ":" + mm + ":" + ss;
+let date = new Date();
+let yyyy = date.getFullYear();
+let mo = date.getMonth()+1;
+if(mo < 10) mo = `0${mo}`;
+let dd = date.getDate();
+if(dd < 10) dd = `0${dd}`;
+let hh = date.getHours();
+if(hh < 10) hh = `0${hh}`;
+let mm = date.getMinutes();
+if(mm < 10) mm = `0${mm}`;
+let ss = date.getSeconds();
+if(ss < 10) ss = `0${ss}`;
+document.getElementById("korea_time").innerHTML = yyyy + "년 " + mo + "월 " + dd + "일<br><br>" + (hh) + " : " + mm + " : " + ss;
 }
 
 const st = window.setInterval(printtime);  // 입장시 시간 출력
