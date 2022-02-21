947c240660db39a9f0d9c2d222c9367d0681036c
Lee J <ljwwnd99@gmail.com>
2022-02-21 14:54:01 +0900

    index person select 수정

--- js/index_reserve.js
+++ js/index_reserve.js
@@ -74,7 +74,7 @@ $(() => {
         $(".reserve_oneway").css({ "display": "none" });
         $(".reserve_multi").css({ "display": "none" });
 
-        
+
         // 예약 관련 레이어 닫기
         e.stopPropagation();
         $(".go_layerbtn").removeClass('close');
@@ -496,105 +496,105 @@ $(() => {
     })
 })
 
-        
-function fnSetPaxCountDown(strPaxType,obj) {
-	var btnParent = $(obj).parents("ul");
 
-	var //
-		iAdultCount = parseInt($(btnParent).find('strong[name=adultPaxCnt]').text()),
-		iChildCount = parseInt($(btnParent).find('strong[name=childPaxCnt]').text()),
-		iInfantCount = parseInt($(btnParent).find('strong[name=infantPaxCnt]').text()),
-		iPaxCount = iAdultCount + iChildCount;
-        
-	switch (strPaxType) {
-		case "ADULT":
-			--iAdultCount;
-			if (iAdultCount >= 0) {
-				--iPaxCount;
-			}
+function fnSetPaxCountDown(strPaxType, obj) {
+    var btnParent = $(obj).parents("ul");
 
-			if (iInfantCount > iAdultCount) {
-				iInfantCount = iAdultCount;
-			}
+    var //
+        iAdultCount = parseInt($(btnParent).find('strong[name=adultPaxCnt]').text()),
+        iChildCount = parseInt($(btnParent).find('strong[name=childPaxCnt]').text()),
+        iInfantCount = parseInt($(btnParent).find('strong[name=infantPaxCnt]').text()),
+        iPaxCount = iAdultCount + iChildCount;
 
-			if (iAdultCount <= 0) {
-				iAdultCount = 0;
-				iInfantCount = 0;
-			}
-			$("strong[name=adultPaxCnt]").text(iAdultCount);
-			$("strong[name=infantPaxCnt]").text(iInfantCount);
-			break;
-		case "CHILD":
-			--iChildCount;
-			if (iChildCount >= 0) {
-				--iPaxCount;
-			}
-			if (iChildCount <= 0) {
-				iChildCount = 0;
-			}
+    switch (strPaxType) {
+        case "ADULT":
+            --iAdultCount;
+            if (iAdultCount >= 0) {
+                --iPaxCount;
+            }
 
-			$("strong[name=childPaxCnt]").text(iChildCount);
-			break;
-		case "INFANT":
-			--iInfantCount;
-			if (iInfantCount <= 0) {
-				iInfantCount = 0;
-			}
+            if (iInfantCount > iAdultCount) {
+                iInfantCount = iAdultCount;
+            }
 
-			$("strong[name=infantPaxCnt]").text(iInfantCount);
-			break;
-	}
+            if (iAdultCount <= 0) {
+                iAdultCount = 0;
+                iInfantCount = 0;
+            }
+            $("strong[name=adultPaxCnt]").text(iAdultCount);
+            $("strong[name=infantPaxCnt]").text(iInfantCount);
+            break;
+        case "CHILD":
+            --iChildCount;
+            if (iChildCount >= 0) {
+                --iPaxCount;
+            }
+            if (iChildCount <= 0) {
+                iChildCount = 0;
+            }
+
+            $("strong[name=childPaxCnt]").text(iChildCount);
+            break;
+        case "INFANT":
+            --iInfantCount;
+            if (iInfantCount <= 0) {
+                iInfantCount = 0;
+            }
+
+            $("strong[name=infantPaxCnt]").text(iInfantCount);
+            break;
+    }
 }
 
-function fnSetPaxCountUp(strPaxType,obj) {
-	var btnParent = $(obj).parents("ul");
+function fnSetPaxCountUp(strPaxType, obj) {
+    var btnParent = $(obj).parents("ul");
 
-	var //
-		iAdultCount = parseInt($(btnParent).find('strong[name=adultPaxCnt]').text()),
-		iChildCount = parseInt($(btnParent).find('strong[name=childPaxCnt]').text()),
-		iInfantCount = parseInt($(btnParent).find('strong[name=infantPaxCnt]').text()),
-		iPaxCount = iAdultCount + iChildCount;
+    var //
+        iAdultCount = parseInt($(btnParent).find('strong[name=adultPaxCnt]').text()),
+        iChildCount = parseInt($(btnParent).find('strong[name=childPaxCnt]').text()),
+        iInfantCount = parseInt($(btnParent).find('strong[name=infantPaxCnt]').text()),
+        iPaxCount = iAdultCount + iChildCount;
 
-	// Count 증감
-	switch (strPaxType) {
-		case "ADULT":
+    // Count 증감
+    switch (strPaxType) {
+        case "ADULT":
             ++iAdultCount;
             ++iPaxCount;
-				$("strong[name=adultPaxCnt]").text(iAdultCount);
-			break;
-		case "CHILD":
+            $("strong[name=adultPaxCnt]").text(iAdultCount);
+            break;
+        case "CHILD":
             ++iChildCount;
             ++iPaxCount;
-				$("strong[name=childPaxCnt]").text(iChildCount);
-			break;
-		case "INFANT":
-			++iInfantCount;
-            if(iInfantCount > iAdultCount){
-                alert('소아는 성인을 넘을 수 없음')
+            $("strong[name=childPaxCnt]").text(iChildCount);
+            break;
+        case "INFANT":
+            ++iInfantCount;
+            if (iInfantCount > iAdultCount && iAdultCount == 0) {
+                alert('소아는 성인 한 명이 같이 동반 해야함!')
                 break;
             }
-			$("strong[name=infantPaxCnt]").text(iInfantCount);
-			break;
-	}
+            $("strong[name=infantPaxCnt]").text(iInfantCount);
+            break;
+    }
 
 }
 
-function submit(){
+function submit() {
 
-    iAdultCount = parseInt($('.person_pop_layer').find('strong[name=adultPaxCnt]').text()),
-    iChildCount = parseInt($('.person_pop_layer').find('strong[name=childPaxCnt]').text()),
-    iInfantCount = parseInt($('.person_pop_layer').find('strong[name=infantPaxCnt]').text())
-    
-    if(iAdultCount > 0 && iChildCount > 0 && iInfantCount >0){
-        $("strong[name=person_num]").text('성인 '+iAdultCount+' 유아 '+iChildCount+' 소아 '+iInfantCount);
-    }else if(iAdultCount > 0 && iChildCount > 0){
-        $("strong[name=person_num]").text('성인 '+iAdultCount+' 유아 '+iChildCount);
-    }else if(iAdultCount > 0 && iInfantCount >0){
-        $("strong[name=person_num]").text('성인 '+iAdultCount+' 소아 '+iInfantCount);
-    }else if(iAdultCount > 0){
-        $("strong[name=person_num]").text('성인 '+iAdultCount);
-    }else if(iChildCount > 0){
-        $("strong[name=person_num]").text('유아 '+iChildCount);
+    iAdultCount = parseInt($('.person_pop_layer').find('strong[name=adultPaxCnt]').text()), // 성인
+        iChildCount = parseInt($('.person_pop_layer').find('strong[name=childPaxCnt]').text()), // 소아
+        iInfantCount = parseInt($('.person_pop_layer').find('strong[name=infantPaxCnt]').text()) // 유아
+
+    if (iAdultCount > 0 && iChildCount > 0 && iInfantCount > 0) {
+        $("strong[name=person_num]").text('성인 ' + iAdultCount + ' 소아 ' + iChildCount + ' 유아 ' + iInfantCount);
+    } else if (iAdultCount > 0 && iChildCount > 0) {
+        $("strong[name=person_num]").text('성인 ' + iAdultCount + ' 소아 ' + iChildCount);
+    } else if (iAdultCount > 0 && iInfantCount > 0) {
+        $("strong[name=person_num]").text('성인 ' + iAdultCount + ' 유아 ' + iInfantCount);
+    } else if (iAdultCount > 0) {
+        $("strong[name=person_num]").text('성인 ' + iAdultCount);
+    } else if (iChildCount > 0) {
+        $("strong[name=person_num]").text('소아 ' + iChildCount);
     }
 
     // 중요. 확인 버튼 누를 때 성인, 소아, 유아의 수를 input:hidden에 넣음
@@ -618,80 +618,80 @@ function submit(){
 
 $(function () {
     $('.infant_modal').hide();
-    $('.baby_info_btn').click(function(){
+    $('.baby_info_btn').click(function () {
         $('.child_modal').hide();
         $('.infant_modal').fadeIn(200);
-        $('.infant_modal').click(function(){
+        $('.infant_modal').click(function () {
             $('.infant_modal').fadeOut(200);
         })
     });
 
     $('.child_modal').hide();
     $('.infant_modal').hide();
-    $('.child_info_btn').click(function(){
+    $('.child_info_btn').click(function () {
         $('.child_modal').fadeIn(200);
-        $('.child_modal').click(function(){
+        $('.child_modal').click(function () {
             $('.child_modal').fadeOut(200);
         })
     });
 })
 
-function comewhatday(){
+function comewhatday() {
     $('.whatday').html('오는날');
 }
-function gowhatday(){
+function gowhatday() {
     $('.whatday').html('가는날');
 }
 
 // 왕복에 넣어주기
-function gowhatdaydd(str){
-    let str1 = str.substr(0,4);
-    let str2 = str.substr(4,2);
-    let str3 = str.substr(6,2);
+function gowhatdaydd(str) {
+    let str1 = str.substr(0, 4);
+    let str2 = str.substr(4, 2);
+    let str3 = str.substr(6, 2);
     $('.go_date_select_optt').val(`${str1}-${str2}-${str3}`);
 }
-function comewhatdaydd(str){
-    let str1 = str.substr(0,4);
-    let str2 = str.substr(4,2);
-    let str3 = str.substr(6,2);
+function comewhatdaydd(str) {
+    let str1 = str.substr(0, 4);
+    let str2 = str.substr(4, 2);
+    let str3 = str.substr(6, 2);
     $('.come_date_select_optt').val(`${str1}-${str2}-${str3}`);
 }
 
 // 편도에 넣어주기
-function gowhatdayddd(str){
-    let str1 = str.substr(0,4);
-    let str2 = str.substr(4,2);
-    let str3 = str.substr(6,2);
+function gowhatdayddd(str) {
+    let str1 = str.substr(0, 4);
+    let str2 = str.substr(4, 2);
+    let str3 = str.substr(6, 2);
     $('.go_date_select_optt').val(`${str1}-${str2}-${str3}`);
 }
 
 // 다구간에 넣어주기
-function gowhatdaydddd(str){
-    let str1 = str.substr(0,4);
-    let str2 = str.substr(4,2);
-    let str3 = str.substr(6,2);
+function gowhatdaydddd(str) {
+    let str1 = str.substr(0, 4);
+    let str2 = str.substr(4, 2);
+    let str3 = str.substr(6, 2);
     $('.go_date_select_optt2').val(`${str1}-${str2}-${str3}`);
 }
 
 
-function minday(){
+function minday() {
     $('.modal_container').fadeIn(200);
     $('.moditext').html('출발일보다 이전 날짜를 선택할 수 없습니다.');
 }
-function departure(){
+function departure() {
     $('.modal_container').fadeIn(200);
     $('.moditext').html('출발지 항목을 입력하세요.');
 }
-function arrive(){
+function arrive() {
     $('.modal_container').fadeIn(200);
     $('.moditext').html('도착지 항목을 입력하세요.');
 }
 
 
-function twochoice(){
-$('.cal').css({"display":"none"});
-$('.cal2').css({"display":"none"});
-$('.cal3').css({"display":"none"});
+function twochoice() {
+    $('.cal').css({ "display": "none" });
+    $('.cal2').css({ "display": "none" });
+    $('.cal3').css({ "display": "none" });
 }
 
 let bbb = 'n';
@@ -714,28 +714,28 @@ $(() => {
         let str = '';
         str = $(this).html();
         $('.arrive_select_optt').val(str);
-        ccc='y';
+        ccc = 'y';
     })
     // 왕복 달력 모달
     $('.go_date').on('click', function () {
-        if(bbb == 'n' && ccc == 'n'){
+        if (bbb == 'n' && ccc == 'n') {
             departure();
-            $('.cal').css({"display":"none"});
-        }else if(bbb == 'y' && ccc == 'n'){
+            $('.cal').css({ "display": "none" });
+        } else if (bbb == 'y' && ccc == 'n') {
             arrive();
-        }else{
-            $('.cal').css({"display":"flex"});
+        } else {
+            $('.cal').css({ "display": "flex" });
             $('.whatday').html('가는날');
         }
     })
     $('.come_date').on('click', function () {
-        if(bbb == 'n' && ccc == 'n'){
+        if (bbb == 'n' && ccc == 'n') {
             departure();
-            $('.cal').css({"display":"none"});
-        }else if(bbb == 'y' && ccc == 'n'){
+            $('.cal').css({ "display": "none" });
+        } else if (bbb == 'y' && ccc == 'n') {
             arrive();
-        }else{
-            $('.cal').css({"display":"flex"});
+        } else {
+            $('.cal').css({ "display": "flex" });
             $('.whatday').html('오는날');
         }
     })
@@ -743,13 +743,13 @@ $(() => {
 
     // 편도 달력 모달
     $('.go_date2').on('click', function () {
-        if(bbb == 'n' && ccc == 'n'){
+        if (bbb == 'n' && ccc == 'n') {
             departure();
-            $('.cal2').css({"display":"none"});
-        }else if(bbb == 'y' && ccc == 'n'){
+            $('.cal2').css({ "display": "none" });
+        } else if (bbb == 'y' && ccc == 'n') {
             arrive();
-        }else{
-            $('.cal2').css({"display":"flex"});
+        } else {
+            $('.cal2').css({ "display": "flex" });
             $('.whatday').html('가는날');
         }
     })
@@ -772,26 +772,26 @@ $(() => {
         let str = '';
         str = $(this).html();
         $('.arrive_select_optt2').val(str);
-        ccc='y';
+        ccc = 'y';
     })
     // 다구간1 달력 모달
     $('.go_date3').on('click', function () {
-        if(bbb == 'n' && ccc == 'n'){
+        if (bbb == 'n' && ccc == 'n') {
             departure();
-            $('.cal3').css({"display":"none"});
-        }else if(bbb == 'y' && ccc == 'n'){
+            $('.cal3').css({ "display": "none" });
+        } else if (bbb == 'y' && ccc == 'n') {
             arrive();
-        }else{
-            $('.cal3').css({"display":"flex"});
+        } else {
+            $('.cal3').css({ "display": "flex" });
             $('.whatday').html('가는날');
         }
     })
 
     // 달력 X 버튼
     $('.x_box').on('click', function () {
-        $('.cal').css({"display":"none"});
-        $('.cal2').css({"display":"none"});
-        $('.cal3').css({"display":"none"});
+        $('.cal').css({ "display": "none" });
+        $('.cal2').css({ "display": "none" });
+        $('.cal3').css({ "display": "none" });
     })
 
     // 모달모음 확인 버튼
